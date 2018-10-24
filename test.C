c80e1a3f30b47c5720f9e435883d4ad484fb2da0
Nikita Ermakov <coffe92@gmail.com>
2018-10-24 22:59:50 +0300

    Import test macros.C

---
+++ test.C
@@ -0,0 +1,93 @@
+R__LOAD_LIBRARY(../StHbtMaker/libStHbtMaker.so)
+R__LOAD_LIBRARY(../StFemtoDst/libStFemto.so)
+
+#include "../StFemtoDst/StHbtFemtoDstReader.h"
+#include "../StHbtMaker/StHbtMaker.h"
+#include "../StHbtMaker/StHbtCorrFctn3DLCMSSym.h"
+#include "../StHbtMaker/StHbtBasicEventCut.h"
+#include "../StHbtMaker/StHbtBasicTrackCut.h"
+#include "../StHbtMaker/StHbtVertexMultAnalysis.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+const double pionMass = 0.139570;
+const double NHitsFitOverNHitsPoss = 0.52;
+const double mSigmaPion[2] = {-2., 2.};
+const double mSigmaExclude[2] = {-2., 2.};
+
+#define OUTERR(p, line) if (!p) { cout << "Can't create " #p " at line " << line << endl; return 1; }
+
+void test(const char *inFile = "CuAu200_test.FemtoDst.root", const char *outFile = "out.root")
+{
+	// Create HBT maker
+	StHbtMaker *hbt = new StHbtMaker();
+	OUTERR(hbt, __LINE__);
+	hbt->setDebug(0);
+	StHbtManager *manager = hbt->hbtManager();
+	OUTERR(manager, __LINE__);
+
+	// Create FemtoDst HBT reader
+	StHbtFemtoDstReader *reader = new StHbtFemtoDstReader("", inFile, "root", -1, false);
+	OUTERR(reader, __LINE__);
+	manager->setEventReader(reader);
+
+	// Create analysis
+        StHbtVertexMultAnalysis *analysis = new StHbtVertexMultAnalysis(/* using default options (see
+                                                                           corresponding .h file) */);
+	OUTERR(analysis, __LINE__);
+	unsigned int why_the_hell_do_you_want_the_constant_lvalue_there_dammit_We_cant_use_rvalue_there = 1e3;
+	analysis->setMinSizePartCollection(why_the_hell_do_you_want_the_constant_lvalue_there_dammit_We_cant_use_rvalue_there);
+	analysis->setNumEventsToMix(4);
+
+	// Create event and track cuts
+	StHbtBasicEventCut *eventCut = new StHbtBasicEventCut();
+	StHbtBasicTrackCut *trackCut = new StHbtBasicTrackCut();
+	StHbtBasicPairCut  *pairCut  = new StHbtBasicPairCut();
+
+	// Add cuts to the analysis
+	analysis->setEventCut(eventCut);
+	analysis->setFirstParticleCut(trackCut);
+	analysis->setSecondParticleCut(trackCut);
+	analysis->setPairCut(pairCut);
+
+	// Add value to tha track
+	trackCut->setMass(pionMass);
+	trackCut->setNHitsFitOverNHitsPoss(NHitsFitOverNHitsPoss);
+	trackCut->setNSigmaPion(mSigmaPion[0], mSigmaPion[1]);
+	trackCut->setNSigmaOther(mSigmaExclude[0], mSigmaExclude[1]);
+	trackCut->setTnTNSigmaPion(mSigmaPion[0], mSigmaPion[1]);
+
+	// Create corr function
+	StHbtCorrFctn3DLCMSSym *cf = new StHbtCorrFctn3DLCMSSym("cf", 40 /* qinv bins */, 0.2 /* qinv high */);
+	OUTERR(reader, __LINE__);
+
+	//  Add corr function to the analysis and analysis to the manager
+	analysis->addCorrFctn(cf);
+	manager->addAnalysis(analysis);
+
+	unsigned long nEvents = 10;//reader->GetNEvents();
+	cout << "Number of events: " << nEvents << endl;
+
+	hbt->Init();
+	while (nEvents--)
+	{
+		int ret = hbt->Make();
+		if (ret)
+		{
+			cout << "StHbtMaker error code: " << ret << " at line " << __LINE__ << endl;
+			break;
+		}
+	}
+	hbt->Finish();
+
+	// Create output file
+	TFile *fOut = new TFile(outFile, "RECREATE");
+	OUTERR(fOut, __LINE__);
+
+	// Write histograms
+	cf->writeOutHistos();
+	fOut->Close();
+}
